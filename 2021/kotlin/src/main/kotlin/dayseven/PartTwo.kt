8dbc6affbd31b6d1785bec2af306f9224c7167df
Michael Fazio <michaelfazio@albert.com>
2021-12-07 08:20:56 -0600

    Day seven - not too bad

---
+++ 2021/kotlin/src/main/kotlin/dayseven/PartTwo.kt
@@ -0,0 +1,35 @@
+package dev.mfazio.aoc.twentyone.dayseven
+
+import dev.mfazio.aoc.twentyone.util.InputHelpers
+
+fun calculateExpensiveCrabFuel(input: List<String>): Int {
+
+    val values = input.map(String::toInt)
+
+    val min = values.minOrNull()
+    val max = values.maxOrNull()
+
+    if (min == null || max == null) return -1
+
+    val groups = (min..max).groupBy { meetingPoint ->
+        values.sumOf {
+            val (mpMin, mpMax) = if (meetingPoint > it) 1 to meetingPoint - it else 1 to it - meetingPoint
+
+            (mpMin..mpMax).sum()
+        }
+    }
+
+    return groups.keys.minOrNull() ?: -1
+}
+
+fun main() {
+    val input = InputHelpers.getListOfStringsFromFile("/dayseven.txt")
+
+    input.firstOrNull()?.split(",")?.let { inputValues ->
+
+        val result = calculateExpensiveCrabFuel(inputValues)
+
+        println(result)
+    } ?: println("No data found!")
+
+}
