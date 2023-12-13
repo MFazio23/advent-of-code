beec2b1bd1012931d723d9721ce7f9ffd4736e8d
Michael Fazio <mfazio23@gmail.com>
2023-12-13 09:57:41 -0600

    Make reflection function generic

--- 2023/kotlin/src/main/kotlin/daythirteen/PartOne.kt
+++ 2023/kotlin/src/main/kotlin/daythirteen/PartOne.kt
@@ -2,7 +2,6 @@ package dev.mfazio.aoc.twentythree.daythirteen
 
 import dev.mfazio.aoc.twentythree.Point
 import dev.mfazio.utils.extensions.getResourceAsListOfStrings
-import kotlin.math.ceil
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -48,60 +47,41 @@ fun findReflectionTotal(block: List<Point<String>>, checkNum: Int = 0): Int {
     val height = block.maxOf { it.y } + 1
     val width = block.maxOf { it.x } + 1
 
-    val horizontalReflection = findHorizontalReflection(block, width, checkNum) ?: 0
-    val verticalReflection = findVerticalReflection(block, height, checkNum) ?: 0
+    val horizontalReflection = findReflection(block, width, checkNum, { it.x }, { it.y }) ?: 0
+    val verticalReflection = findReflection(block, height, checkNum, { it.y }, { it.x }) ?: 0
 
     return (horizontalReflection) + (100 * (verticalReflection))
 }
 
-fun findHorizontalReflection(block: List<Point<String>>, width: Int, checkNum: Int = 0): Int? =
-    (0..width - 2).firstNotNullOfOrNull { column ->
-        val (left, right) = block.partition { it.x <= column }
+fun findReflection(
+    block: List<Point<String>>,
+    size: Int,
+    checkNum: Int = 0,
+    splitFunction: (Point<String>) -> Int,
+    groupFunction: (Point<String>) -> Int,
+): Int? =
+    (0..size - 2).firstNotNullOfOrNull { index ->
+        val (a, b) = block.partition { splitFunction(it) <= index }
 
-        val isLeftHalf = left.size <= right.size
+        val isFirstHalf = a.size <= b.size
 
-        val leftSize = column + 1
-        val rightSize = width - leftSize
-        val actualLeft = if (isLeftHalf) left else left.filter { (width - rightSize * 2 until width).contains(it.x) }
-        val actualRight = if (isLeftHalf) right.filter { (leftSize until leftSize * 2).contains(it.x) } else right
+        val firstSize = index + 1
+        val secondSize = size - firstSize
+        val actualFirst = if (isFirstHalf) a else a.filter { (size - secondSize * 2 until size).contains(splitFunction(it)) }
+        val actualSecond = if (isFirstHalf) b.filter { (firstSize until firstSize * 2).contains(splitFunction(it)) } else b
 
-        val leftRows = actualLeft.groupBy { it.y }
-        val rightRows = actualRight.groupBy { it.y }
+        val firstRows = actualFirst.groupBy(groupFunction)
+        val secondRows = actualSecond.groupBy(groupFunction)
 
-        val differences = leftRows.map { (y, leftPoints) ->
-            rightRows[y]?.let { rightPoints ->
-                leftPoints.joinToString("") { it.data } to rightPoints.reversed().joinToString("") { it.data }
-            }?.let { (left, right) ->
-                left.zip(right).count { (l, r) -> l != r }
+        val differences = firstRows.map { (ind, firstPoints) ->
+            secondRows[ind]?.let { secondPoints ->
+                firstPoints.joinToString("") { it.data } to secondPoints.reversed().joinToString("") { it.data }
+            }?.let { (first, second) ->
+                first.zip(second).count { (f, s) -> f != s }
             } ?: 0
         }
 
-        if (differences.sum() == checkNum) leftSize else null
-    }
-
-fun findVerticalReflection(block: List<Point<String>>, height: Int, checkNum: Int = 0): Int? =
-    (0..height - 2).firstNotNullOfOrNull { row ->
-        val (top, bottom) = block.partition { it.y <= row }
-
-        val isTopHalf = top.size < bottom.size
-
-        val topSize = row + 1
-        val bottomSize = height - topSize
-        val actualTop = if (isTopHalf) top else top.filter { (height - bottomSize * 2 until height).contains(it.y) }
-        val actualBottom = if (isTopHalf) bottom.filter { (topSize until topSize * 2).contains(it.y) } else bottom
-
-        val topColumns = actualTop.groupBy { it.x }
-        val bottomColumns = actualBottom.groupBy { it.x }
-
-        val differences = topColumns.map { (x, topPoints) ->
-            bottomColumns[x]?.let { bottomPoints ->
-                topPoints.joinToString("") { it.data } to bottomPoints.reversed().joinToString("") { it.data }
-            }?.let { (top, bottom) ->
-                top.zip(bottom).count { (l, r) -> l != r }
-            } ?: 0
-        }
-
-        if (differences.sum() == checkNum) topSize else null
+        if (differences.sum() == checkNum) firstSize else null
     }
 
 fun printBlock(block: List<Point<String>>) {
